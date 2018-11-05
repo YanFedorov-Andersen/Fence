71be257ca5d2eb907269014690670c91dfe2547e
yan <axxxx@ukr.net>
2018-11-05 10:14:09 +0200

    add models

---
+++ Fence.DataAccess/UserStoreProceduresAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fence.DataAccess
+{
+    public class UserStoreProceduresAccess
+    {
+        private readonly UserAndMessagesEntities _modelContext;
+        public UserStoreProceduresAccess(UserAndMessagesEntities modelContext)
+        {
+            _modelContext = modelContext;
+        }
+
+        public List<UserEntity> GetUsers()
+        {
+            var result = _modelContext.Database.SqlQuery<UserEntity>("EXEC GetAllUsers");
+            var usersList = result.ToListAsync().Result;
+            return usersList;
+        }
+
+        public List<UserEntity> UpdateUser(UserEntity user)
+        {
+            var result = _modelContext.Database.SqlQuery<UserEntity>("EXEC UpdateUserEntity", new SqlParameter()
+                );
+            var usersList = result.ToListAsync().Result;
+            return usersList;
+        }
+    }
+}
---
+++ Fence.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fence.WebApi.WebApiApplication" Language="C#" %>
